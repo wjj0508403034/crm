dc1cc53d5bd5cee6991fa9f99fb7476512a203d9
GLOBAL\i076641 <jingjing.wang01@sap.com>
2019-08-27 12:51:01 +0800

    update

---
+++ crm/src/resources/static/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var huoyun = angular.module('huoyun', ["huoyun.widgets", 'ui.router']);
+
+huoyun.controller("appController", ["$scope", function ($scope) {
+  $scope.controller = {
+    sidebar: new HuoYunWidgets.Controls.SideBar({
+      items: [{
+        text: "任务",
+        items: [{
+          text: "我的任务"
+        }, {
+          text: "任务管理",
+          selected: true
+        }, {
+          text: "任务审核"
+        }]
+      }, {
+        text: "视频",
+        items: [{
+          text: "上传视频"
+        }, {
+          text: "视频管理"
+        }]
+      }, {
+        text: "用户",
+        items: [{
+          text: "个人信息"
+        }, {
+          text: "重设密码"
+        }, {
+          text: "用户管理"
+        }]
+      }]
+    })
+  };
+}]);
